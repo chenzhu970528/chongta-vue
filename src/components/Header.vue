19e5a1f4df114b0c39b0d7bf4d45a14098581e84
zc <30995730@qq.com>
2018-10-22 00:44:24 +0800

    10.21 later1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
         <!--exact可以避免根路由使用始终激活的样式-->
         <router-link tag="li" active-class="active" role="presentation" to="/" exact><a>首页</a></router-link>
         <router-link tag="li" active-class="active" role="presentation" to="/adoption"><a>领养中心</a></router-link>
-        <router-link tag="li" active-class="active" role="presentation" to="/homeless"><a>流浪精灵</a></router-link>
+        <router-link tag="li" active-class="active" role="presentation" to="/homeless"><a>流浪精灵/寻宠之家</a></router-link>
         <router-link tag="li" active-class="active" role="presentation" to="/matchmaking"><a>宠物婚介</a></router-link>
         <router-link tag="li" active-class="active" role="presentation" to="/forum"><a>宠宠交流</a></router-link>
       </ul>
