77e89eb81417c8e3ad0bb7822b9dd7774f358489
leslie <707296377@qq.com>
2018-10-21 16:24:48 +0800

    改导航栏

--- src/components/adoption/AdoptDetails.vue
+++ src/components/adoption/AdoptDetails.vue
@@ -70,7 +70,7 @@
 <style scoped>
   .cont{
     background:rgba(255,255,255,0.9);
-    margin-top:50px ;
+    margin-top:100px ;
     box-shadow:-2px 2px 10px 2px #bcbcbc;
     min-height: 500px;
     width: 1240px;
